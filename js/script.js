8ced7959aaf46f954210aea830b37f2188a471a8
Sebastian-Agrano <sebastianagrano@outlook.com>
2024-05-17 10:57:27 -0300

    update css

--- js/script.js
+++ js/script.js
@@ -1,50 +1,46 @@
-const accordionList = document.getElementsByClassName('accordion');
-const faqList = document.getElementsByClassName('faq');
-const boxImage = document.querySelector('.img-desk-box');
+const accordionList = document.getElementsByClassName("accordion");
+const faqList = document.getElementsByClassName("faq");
+const boxImage = document.querySelector(".img-desk-box");
 
 let flag = {
-    actual: 0,
-    previous: 0
-}
+  actual: 0,
+  previous: 0,
+};
 
-const toggleElements = position => {
-    faqList[position].childNodes[1].classList.toggle('bold-tittle');
-    faqList[position].childNodes[3].classList.toggle('show-info');
-    accordionList[position].childNodes[3].classList.toggle('image-rotate');
-    toggleBox(faqList);
-}
+const toggleElements = (position) => {
+  faqList[position].childNodes[1].classList.toggle("bold-title");
+  faqList[position].childNodes[3].classList.toggle("show-info");
+  accordionList[position].childNodes[3].classList.toggle("image-rotate");
+  toggleBox(faqList);
+};
 
 const countClass = (list) => {
-    let count = 0;
+  let count = 0;
 
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].childNodes[1].classList.contains('bold-tittle')) {
-            count++;
-        }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].childNodes[1].classList.contains("bold-title")) {
+      count++;
     }
+  }
 
-    flag.previous = flag.actual;
-    flag.actual = count;    
-}
+  flag.previous = flag.actual;
+  flag.actual = count;
+};
 
-const toggleBox = list => {
+const toggleBox = (list) => {
+  countClass(list);
 
-    countClass(list);
-    
-    if (flag.actual === 0) {
-        boxImage.classList.remove('box-traslate');
-    }
+  if (flag.actual === 0) {
+    boxImage.classList.remove("box-traslate");
+  }
 
-    if (flag.actual === 1 && flag.previous === 0) {
-        boxImage.classList.add('box-traslate');
-    }
-}
-
+  if (flag.actual === 1 && flag.previous === 0) {
+    boxImage.classList.add("box-traslate");
+  }
+};
 
 for (let i = 0; i < accordionList.length; i++) {
-    accordionList[i].addEventListener('click', () => {
-        toggleElements(i);
-    });
+  accordionList[i].addEventListener("click", () => {
+    toggleElements(i);
+  });
 }
-
-
